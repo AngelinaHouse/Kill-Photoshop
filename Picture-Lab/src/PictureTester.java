3bc352c43a36324c401d7e260d40b48dc17bca81
angelina.house <angelina.house@MU-C1-19.mullen.pvt.k12.co.us>
2021-03-25 08:55:29 -0600

    A5 3&4

--- Picture-Lab/src/PictureTester.java
+++ Picture-Lab/src/PictureTester.java
@@ -17,6 +17,22 @@ public class PictureTester
     beach.explore();
   }
   
+  public static void testKeepOnlyBlue()
+  {
+	Picture beach = new Picture("beach.jpg");
+    beach.explore();
+    beach.keepOnlyBlue();
+    beach.explore();
+  }
+  
+  public static void testNegate()
+  {
+	 Picture beach = new Picture("beach.jpg");
+	 beach.explore();
+	 beach.negate();
+	 beach.explore();
+  }
+  
   /** Method to test mirrorVertical */
   public static void testMirrorVertical()
   {
@@ -58,11 +74,11 @@ public class PictureTester
     // uncomment a call here to run a test
     // and comment out the ones you don't want
     // to run
-    testZeroBlue();
+    //testZeroBlue();
     //testKeepOnlyBlue();
     //testKeepOnlyRed();
     //testKeepOnlyGreen();
-    //testNegate();
+    testNegate();
     //testGrayscale();
     //testFixUnderwater();
     //testMirrorVertical();
